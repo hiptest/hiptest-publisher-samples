89c6605da858dd0cfcab075fed5ff40390a5b4ec
Vincent Pretre <v.pretre@smartesting.com>
2014-05-23 18:20:58 +0200

    Added Travis config, should update and run tests in every languages Typo in bootstrap.py name

---
+++ run_travis.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ruby
+zest-publisher -c rspec.conf --tests-only
+bundle install
+bundle exec rspec
+
+cd ../python
+zest-publisher -c unittest.conf --tests-only
+python bootstrap.py
+bin/buildout
+bin/test
