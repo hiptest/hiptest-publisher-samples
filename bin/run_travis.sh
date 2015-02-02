49cc850e53d49b21709aca5bdbc2377349f11c12
Vincent Pretre <v.pretre@smartesting.com>
2015-02-02 09:43:17 +0100

    Enable selecting language and framework for execution

--- bin/run_travis.sh
+++ bin/run_travis.sh
@@ -14,33 +14,85 @@ logMessage() {
   echo "-------------------------------------------------------------------------------"
 }
 
-gem install hiptest-publisher
+seekAndRunTestsForDir() {
+  local dir=$1;
 
-for dir in */; do
-  cd $dir
-  header $dir
+  if [ "$dir" != "bin/" ]; then
+    cd $dir
+    header $dir
 
-  for src in *_htp.sh; do
-    if [ -f $src ]; then
-      setup() {
-        :
-      };
+    for src in *_htp.sh; do
+      if [ -f $src ]; then
+        updateAndRunTests $src;
+      fi
+    done
 
-      source $src
+    cd ..
+  fi
+}
 
-      logMessage "Setting up project"
-      setup
+updateAndRunTests() {
+  local src=$1;
+  setup() {
+    :
+  };
 
-      logMessage "Updating tests"
-      hiptest-publisher -c $hiptest_config --tests-only
+  source $src
 
-      logMessage "Running tests"
-      run_tests
+  logMessage "Setting up project"
+  setup
 
-      logMessage "Pushing results to Hiptest"
-      hiptest-publisher -p $results -c $hiptest_config
-    fi
-  done
+  logMessage "Updating tests"
+  hiptest-publisher -c $hiptest_config --tests-only
 
-  cd ..
+  logMessage "Running tests"
+  run_tests
+
+  logMessage "Pushing results to Hiptest"
+  hiptest-publisher -p $results -c $hiptest_config
+}
+
+installHiptestPublisher() {
+  gem install hiptest-publisher
+}
+
+show_help() {
+  echo "bin/run_travis"
+  echo " -l Use specific language"
+  echo " -f Use specific htp file (only use framework name (eg: minitest, not minitest_htp.sh)"
+  echo " -h Show this message"
+}
+
+
+OPTIND=1
+export language=""
+export htp_file=""
+
+while getopts "h?l:f:" opt; do
+  case "$opt" in
+  l)  language="$OPTARG"
+      ;;
+  f)  htp_file="$OPTARG"
+      ;;
+  h|\?)
+      show_help
+      exit 0
+      ;;
+  esac
 done
+
+installHiptestPublisher
+
+if [ "$language" != "" ]; then
+  if [ "$htp_file" != "" ]; then
+    cd $language
+    updateAndRunTests "${htp_file}_htp.sh"
+    cd ..
+  else
+    seekAndRunTestsForDir $language
+  fi
+else
+  for dir in */; do
+    seekAndRunTestsForDir $dir
+  done
+fi
