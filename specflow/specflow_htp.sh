492a958fed46cb09633a821f86d7632962f3e33f
Vincent Pretre <vincent.pretre@gmail.com>
2015-11-19 11:40:18 +0100

    Use correct path to specflow

--- specflow/specflow_htp.sh
+++ specflow/specflow_htp.sh
@@ -6,7 +6,7 @@ setup() {
 }
 
 run_tests() {
-  mono SpecFlow_v1.9.0_bin/tools/specflow.exe generateall specflow_samples.csproj
-  mcs /target:library /out:hiptest.publisher.samples.dll ../csharp/class/CoffeeMachine.cs features/Actionwords.cs features/Error_messages.feature.cs features/Scenarios.feature.cs -reference:nunit.framework.dll -r:SpecFlow_v1.9.0_bin/tools/TechTalk.SpecFlow.dll
+  mono SpecFlow.1.9.0/tools/specflow.exe generateall specflow_samples.csproj
+  mcs /target:library /out:hiptest.publisher.samples.dll ../csharp/class/CoffeeMachine.cs features/Actionwords.cs features/Error_messages.feature.cs features/Scenarios.feature.cs -reference:nunit.framework.dll -r:SpecFlow.1.9.0/tools/TechTalk.SpecFlow.dll
   nunit-console hiptest.publisher.samples.dll
 }
