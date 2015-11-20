370ff577f8157a4b8bf3931e30eeafa85c1f72e6
Vincent Pretre <vincent.pretre@gmail.com>
2015-11-20 19:31:10 +0100

    Ruby/minitest is also using the new BDD scenarios now

--- ruby/test/actionwords.rb
+++ ruby/test/actionwords.rb
@@ -7,46 +7,66 @@ module Actionwords
     @sut ||= CoffeeMachine.new
   end
 
-  def start_the_coffee_machine(lang = 'en')
+  def i_start_the_coffee_machine(lang = 'en')
     sut.start(lang)
   end
 
-  def shutdown_coffee_machine
+  def i_shutdown_the_coffee_machine
     sut.stop
   end
 
-  def assert_displayed_message(message)
+  def message_message_should_be_displayed(message)
     assert_equal(sut.message, message)
   end
 
-  def assert_coffee_served
+  def coffee_should_be_served
     assert(sut.coffee_served)
   end
 
-  def assert_no_coffee_is_served
+  def coffee_should_not_be_served
     refute(sut.coffee_served)
   end
 
-  def take_a_coffee
+  def i_take_a_coffee
     sut.take_coffee
   end
 
-  def empty_coffee_grounds
+  def i_empty_the_coffee_grounds
     sut.empty_grounds
   end
 
-  def fill_beans
+  def i_fill_the_beans_tank
     sut.fill_beans
   end
 
-  def fill_water_tank
+  def i_fill_the_water_tank
     sut.fill_tank
   end
 
-  def take_coffees(coffee_number = 10)
+  def i_take_coffee_number_coffees(coffee_number = 10)
     while (coffee_number > 0)
-      take_a_coffee
+      i_take_a_coffee
       coffee_number = coffee_number - 1
     end
+  end
+
+  def the_coffee_machine_is_started
+    i_start_the_coffee_machine
+  end
+
+  def fifty_coffees_have_been_taken_without_filling_the_tank
+    i_take_coffee_number_coffees(30)
+    i_fill_the_beans_tank
+    i_empty_the_coffee_grounds
+    i_take_coffee_number_coffees(20)
+    i_fill_the_beans_tank
+    i_empty_the_coffee_grounds
+  end
+
+  def thirty_eight_coffees_are_taken_without_filling_beans
+    i_take_coffee_number_coffees(37)
+    i_empty_the_coffee_grounds
+    i_fill_the_water_tank
+    i_take_a_coffee
   end
 end
--- ruby/test/project_test.rb
+++ ruby/test/project_test.rb
@@ -7,13 +7,12 @@ class TestCoffeeMachineHiptestPublisherSample < MiniTest::Unit::TestCase
   include Actionwords
 
   def simple_use(lang, ready_message)
-    assert_displayed_message("")
-    start_the_coffee_machine(lang)
-    assert_displayed_message(ready_message)
-    take_a_coffee
-    assert_coffee_served
-    shutdown_coffee_machine
-    assert_displayed_message("")
+    # Given I start the coffee machine "<lang>"
+    i_start_the_coffee_machine(lang)
+    # When I take a coffee
+    i_take_a_coffee
+    # Then coffee should be served
+    coffee_should_be_served
   end
 
   def test_Simple_use_english_uid70bd316f778e4c4db0d3b6b02fd94bfb
@@ -26,55 +25,95 @@ class TestCoffeeMachineHiptestPublisherSample < MiniTest::Unit::TestCase
 
 
   def test_Full_grounds_does_not_block_coffee_uid34b48113ba1148c782717dbedcbac71f
-    start_the_coffee_machine
-    take_coffees(29)
-    assert_displayed_message("Ready")
-    take_a_coffee
-    assert_coffee_served
-    assert_displayed_message("Empty grounds")
-    fill_water_tank
-    fill_beans
-    take_coffees(20)
-    assert_coffee_served
-    assert_displayed_message("Empty grounds")
+    # Given the coffee machine is started
+    the_coffee_machine_is_started
+    # When I take "29" coffees
+    i_take_coffee_number_coffees(29)
+    # Then message "Ready" should be displayed
+    message_message_should_be_displayed("Ready")
+    # When I take a coffee
+    i_take_a_coffee
+    # Then coffee should be served
+    coffee_should_be_served
+    # And message "Empty grounds" should be displayed
+    message_message_should_be_displayed("Empty grounds")
+    # When I fill the water tank
+    i_fill_the_water_tank
+    # And I fill the beans tank
+    i_fill_the_beans_tank
+    # And I take "20" coffees
+    i_take_coffee_number_coffees(20)
+    # Then coffee should be served
+    coffee_should_be_served
+    # And message "Empty grounds" should be displayed
+    message_message_should_be_displayed("Empty grounds")
   end
 
   def test_Water_runs_away_uidb5bce658f38446b486c30cc74694013e
-    start_the_coffee_machine
-    take_coffees(30)
-    fill_beans
-    empty_coffee_grounds
-    take_coffees(20)
-    fill_beans
-    empty_coffee_grounds
-    assert_displayed_message("Fill tank")
-    fill_beans
-    take_a_coffee
-    assert_coffee_served
-    take_coffees(9)
-    take_a_coffee
-    assert_no_coffee_is_served
-    assert_displayed_message("Fill tank")
-    fill_water_tank
-    empty_coffee_grounds
-    assert_displayed_message("Ready")
+    # Given the coffee machine is started
+    the_coffee_machine_is_started
+    # When fifty coffees have been taken without filling the tank
+    fifty_coffees_have_been_taken_without_filling_the_tank
+    # Then message "Fill tank" should be displayed
+    message_message_should_be_displayed("Fill tank")
+    # When I take a coffee
+    i_take_a_coffee
+    # Then coffee should be served
+    coffee_should_be_served
+    # When I take "10" coffees
+    i_take_coffee_number_coffees(10)
+    # Then coffee should not be served
+    coffee_should_not_be_served
+    # And message "Fill tank" should be displayed
+    message_message_should_be_displayed("Fill tank")
+    # When I fill the water tank
+    i_fill_the_water_tank
+    # Then message "Ready" should be displayed
+    message_message_should_be_displayed("Ready")
   end
 
   def test_Beans_run_out_uid1c7b3ec4bc8f4723a50deeef11c476d3
-    start_the_coffee_machine
-    assert_displayed_message("Ready")
-    take_coffees(37)
-    empty_coffee_grounds
-    fill_water_tank
-    assert_displayed_message("Ready")
-    take_a_coffee
-    assert_coffee_served
-    assert_displayed_message("Fill beans")
-    take_a_coffee
-    take_a_coffee
-    assert_coffee_served
-    assert_displayed_message("Fill beans")
-    take_a_coffee
-    assert_no_coffee_is_served
+    # Given the coffee machine is started
+    the_coffee_machine_is_started
+    # When thirty eight coffees are taken without filling beans
+    thirty_eight_coffees_are_taken_without_filling_beans
+    # Then coffee should be served
+    coffee_should_be_served
+    # And message "Fill beans" should be displayed
+    message_message_should_be_displayed("Fill beans")
+    # When I take "2" coffees
+    i_take_coffee_number_coffees(2)
+    # Then coffee should be served
+    coffee_should_be_served
+    # And message "Fill beans" should be displayed
+    message_message_should_be_displayed("Fill beans")
+    # When I take a coffee
+    i_take_a_coffee
+    # Then coffee should not be served
+    coffee_should_not_be_served
+  end
+
+  def test_No_messages_are_displayed_when_machine_is_shut_down_uid7cdba6eb9c0947fe8eb14834e5dad3b2
+    # Given the coffee machine is started
+    the_coffee_machine_is_started
+    # When I shutdown the coffee machine
+    i_shutdown_the_coffee_machine
+    # Then message "" should be displayed
+    message_message_should_be_displayed("")
+  end
+
+  def messages_are_based_on_language(lang, ready_message)
+    # When I start the coffee machine "<lang>"
+    i_start_the_coffee_machine(lang)
+    # Then message "<ready_message>" should be displayed
+    message_message_should_be_displayed(ready_message)
+  end
+
+  def test_Messages_are_based_on_language_english_uidcb698f1ea38c4feb9087e8fa7e971aee
+    messages_are_based_on_language('en', 'Ready')
+  end
+
+  def test_Messages_are_based_on_language_french_uid4dddcafb9a9a49eab84f762039b96ef4
+    messages_are_based_on_language('fr', 'Pret')
   end
 end
