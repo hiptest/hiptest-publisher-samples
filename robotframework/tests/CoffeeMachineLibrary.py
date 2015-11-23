9de1bf935115f61422e7d32d6fe3d1f9490372ff
Vincent Pretre <vincent.pretre@gmail.com>
2015-11-23 09:56:32 +0100

    Robot framework is also using latest definitions

--- robotframework/tests/CoffeeMachineLibrary.py
+++ robotframework/tests/CoffeeMachineLibrary.py
@@ -4,38 +4,38 @@ class CoffeeMachineLibrary(object):
     def __init__(self):
         self.sut = CoffeeMachine()
 
-    def start_the_coffee_machine(self, lang = 'en'):
+    def i_start_the_coffee_machine(self, lang = 'en'):
         self.sut.start(lang)
 
-    def shutdown_coffee_machine(self):
+    def i_shutdown_the_coffee_machine(self):
         self.sut.stop()
 
-    def assert_displayed_message(self, message = ''):
+    def message_message_should_be_displayed(self, message = ''):
         if self.sut.message != message:
             raise AssertionError('%s != %s' % (self.sut.message, message))
 
-    def assert_coffee_served(self):
+    def coffee_should_be_served(self):
         if not self.sut.coffee_served:
             raise AssertionError('No coffee served')
 
-    def assert_no_coffee_is_served(self):
+    def coffee_should_not_be_served(self):
         if self.sut.coffee_served:
             raise AssertionError('A coffee is served')
 
-    def take_a_coffee(self):
+    def i_take_a_coffee(self):
         self.sut.take_coffee()
 
-    def empty_coffee_grounds(self):
+    def i_empty_the_coffee_grounds(self):
         self.sut.empty_grounds()
 
-    def fill_beans(self):
+    def i_fill_the_beans_tank(self):
         self.sut.fill_beans()
 
-    def fill_water_tank(self):
+    def i_fill_the_water_tank(self):
         self.sut.fill_tank()
 
-    def take_coffees(self, coffee_number = 10):
+    def i_take_coffee_number_coffees(self, coffee_number = 10):
         coffee_number = int(coffee_number)
         while (coffee_number > 0):
-            self.take_a_coffee()
+            self.i_take_a_coffee()
             coffee_number = coffee_number - 1
